a77d8f63aa5668761d0b9595794e5ecf8ec4f9dc
Wagner Kauan <155324620+WagnerKauan@users.noreply.github.com>
2025-08-20 01:09:45 -0300

    style:  centralizando conteudo do header e adicionando sombra

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -21,86 +21,82 @@ export function Header() {
   ];
 
   return (
-    <header className="flex items-center 
-      justify-between shadow-md px-4 py-6 fixed top-0 left-0 right-0 z-[300] bg-white">
-      <Sheet>
-        <div className="w-[111px] h-[48px] relative">
-          <Image
-            src={"/image/logoTrentine 1.png"}
-            alt="Logo da trentine"
-            className="w-full object-cover"
-            fill
-            quality={100}
-            priority
-          />
-        </div>
-
-        <nav className="hidden md:flex items-center gap-7.5">
-          {sections.map((section) => (
-            <SideBarLink
-              key={section.href}
-              href={section.href}
-              label={section.label}
+    <div className="fixed top-0 left-0 right-0 z-[300] bg-white shadow-md">
+      <header className="flex items-center
+        justify-between px-4 py-6 container mx-auto">
+        <Sheet>
+          <div className="w-[111px] h-[48px] relative">
+            <Image
+              src={"/image/logoTrentine 1.png"}
+              alt="Logo da trentine"
+              className="w-full object-cover"
+              fill
+              quality={100}
+              priority
             />
-          ))}
-        </nav>
-
-        <div className="flex items-center gap-7.5 flex-1 justify-end 
-          md:justify-items-start mr-7.5 md:mr-0 md:flex-initial">
-          <div className="relative">
-            <ShoppingCart className="w-6 h-6" />
-            <span className="absolute -top-1 left-4 w-4 h-4 bg-primary-normal rounded-full flex 
-              items-center justify-center text-[10px] text-white">
-              3
-            </span>
           </div>
-
-          <Button
-            className="hidden cursor-pointer md:flex bg-primary-normal
-             hover:bg-primary-dark duration-300 px-8 py-5"
-          >
-            Pedir agora
-          </Button>
-        </div>
-
-        {/* sidebar Mobile */}
-        <div className="md:hidden flex items-center gap-10">
-          <div className="relative md:hidden">
-            <SheetTrigger asChild>
-              <Menu className="w-6 h-6" />
-            </SheetTrigger>
-
-            <SheetContent side="left" className="max-w-[230px] z-[300]">
-              <SheetHeader>
-                <SheetTitle>
-                  <div className="w-[111px] h-[48px] relative">
-                    <Image
-                      src={"/image/logoTrentine 1.png"}
-                      alt="Logo da trentine"
-                      className="w-full object-cover"
-                      fill
-                      quality={100}
-                      priority
+          <nav className="hidden md:flex items-center gap-7.5">
+            {sections.map((section) => (
+              <SideBarLink
+                key={section.href}
+                href={section.href}
+                label={section.label}
+              />
+            ))}
+          </nav>
+          <div className="flex items-center gap-7.5 flex-1 justify-end
+            md:justify-items-start mr-7.5 md:mr-0 md:flex-initial">
+            <div className="relative">
+              <ShoppingCart className="w-6 h-6" />
+              <span className="absolute -top-1 left-4 w-4 h-4 bg-primary-normal rounded-full flex
+                items-center justify-center text-[10px] text-white">
+                3
+              </span>
+            </div>
+            <Button
+              className="hidden cursor-pointer md:flex bg-primary-normal
+               hover:bg-primary-dark duration-300 px-8 py-5"
+            >
+              Pedir agora
+            </Button>
+          </div>
+          {/* sidebar Mobile */}
+          <div className="md:hidden flex items-center gap-10">
+            <div className="relative md:hidden">
+              <SheetTrigger asChild>
+                <Menu className="w-6 h-6" />
+              </SheetTrigger>
+              <SheetContent side="left" className="max-w-[230px] z-[300]">
+                <SheetHeader>
+                  <SheetTitle>
+                    <div className="w-[111px] h-[48px] relative">
+                      <Image
+                        src={"/image/logoTrentine 1.png"}
+                        alt="Logo da trentine"
+                        className="w-full object-cover"
+                        fill
+                        quality={100}
+                        priority
+                      />
+                    </div>
+                  </SheetTitle>
+                  <SheetDescription></SheetDescription>
+                </SheetHeader>
+                <nav className="flex flex-col gap-7 px-5">
+                  {sections.map((section) => (
+                    <SideBarLink
+                      key={section.href}
+                      href={section.href}
+                      label={section.label}
                     />
-                  </div>
-                </SheetTitle>
-                <SheetDescription></SheetDescription>
-              </SheetHeader>
-
-              <nav className="flex flex-col gap-7 px-5">
-                {sections.map((section) => (
-                  <SideBarLink
-                    key={section.href}
-                    href={section.href}
-                    label={section.label}
-                  />
-                ))}
-              </nav>
-            </SheetContent>
+                  ))}
+                </nav>
+              </SheetContent>
+            </div>
           </div>
-        </div>
-      </Sheet>
-    </header>
+        </Sheet>
+      </header>
+    </div>
   );
 }
 
