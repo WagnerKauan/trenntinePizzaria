328569b4fad4d89fcd7db368c77ea42235ff09a9
Wagner Kauan <155324620+WagnerKauan@users.noreply.github.com>
2025-09-13 17:07:41 -0300

    chore: Configurando cache da página de checkout

--- src/app/(public)/checkout/page.tsx
+++ src/app/(public)/checkout/page.tsx
@@ -1,10 +1,9 @@
-"use server";
-
 import { redirect } from "next/navigation";
 import { getStatusPizzaria } from "../_data-access/get-status-pizzaria";
 import { CheckoutContent } from "./_components/checkout-content";
 
 
+export const revalidate = 0;
 
 export default async function Checkout() {
 
